ba44ed649580c3cba08e5ec7a59201426b16c308
Aleksandr Chubar <aleksandr.chubar@gmail.com>
2021-10-24 18:11:51 +0300

    готово

--- src/com/pb/chubar/hw3/Array.java
+++ src/com/pb/chubar/hw3/Array.java
@@ -4,58 +4,60 @@ import java.util.Scanner;
 
 public class Array {
     public static void main(String[] args) {
-        int sum = 0;
-        int sizeI = 10;
+        int sum = 0;                    //Переменная сумма
+        int sizeI = 10;                 //Размер массива
         int[] array = new int [sizeI];
-        int countPlus = 0;
-        int countMinus = 0;
-        int countZero = 0;
-        //5int zero = zero[0];
+        int countPlus = 0;              //Счетчики положительных
+        int countMinus = 0;             //Счетчики отрицательны
+        int countZero = 0;              //Счетчики нулевых значений
+
 
         // Цикл ввода значений в массив
         for (int i=0; i < sizeI; i++){
-            System.out.println("Введите значение " + (i+1) + " ячейки массива:");
+            System.out.println("Введите значение " + (i+1) + " ячейки массива:"); //+1 для отображения пользователю
             Scanner scan = new Scanner(System.in);
             array[i] = scan.nextInt();
         }
 
         // Отображение всех внесенных данных для перепроверки
+        System.out.print("\nВы ввели такие значения:\n");
         for (int i=0; i < sizeI; i++) {
         //    System.out.print("Ячейка " + (i+1) + "; Значение:" + array[i] );
-            System.out.print(" " + array[i] );              //Вывод всех значений
 
+            System.out.print(" " + array[i] );              //Вывод всех значений
             sum = sum + array[i];                           //Подсчет суммы
-        //    System.out.print("; Сумма = " + sum);         //Вывод суммы (для проверки)
 
             //Подсчет положительных, отрицательных и нулевых чисел
             if (array [i]<0){ countMinus++;  }
             else if (array [i]==0) { countZero++;}
             else if (array[i] > 0) { countPlus++; }
-
-
-
         }
         System.out.print("\n" + "Положительных: " + countPlus + ";\nОтрицательных: " + countMinus + ";\nНулевые: " + countZero);
-        System.out.print("; \nСумма итого = " + sum + ".");               //Итоговая сумма
+        System.out.print("; \nСумма итого = " + sum + ".\n");       //Вывод итоговой суммы всех значений
+
+
         // Сортировка
-        // ЕЩЕ НЕ ДОДЕЛАЛ ! ! !
+        boolean isSorted = false;
+        while (!isSorted)
+        {
+            isSorted=true;
+            for (int i=1; i < sizeI; i++)
+            {
+                if (array[i] < array[i - 1])
+                {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //Вывод уже отсортированного массива
+        System.out.print("\nОтсортированный массив:\n");
+        for (int i=0; i < sizeI; i++)
+        {
+            System.out.print(" " + array[i] );
+        }
     }
 }
